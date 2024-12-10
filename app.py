c466a1aefe6a79ddb594427e2f5b0fa67b6b4835
Pradipa Y <merakayamsstraat@gmail.com>
2024-12-10 08:31:23 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    user_input = request.form.get('user_input')
+    return f"Kamu Mengirimkan : {user_input}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
